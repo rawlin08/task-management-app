cc2ae60cfdabe98af41f260e9f50838ca34be93a
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 23:02:55 -0500

    adding text, disabled buttons

--- src/app/components/heading.component.ts
+++ src/app/components/heading.component.ts
@@ -6,13 +6,13 @@ import { AppComponent } from '../app.component';
   template: `
   <div class="left">
     <img src="assets/images/logo-mobile.svg" alt="">
-    <button mat-button (menuOpened)="this.chevronIcon = 'up'" (menuClosed)="this.chevronIcon = 'down'" [matMenuTriggerFor]="menu">{{ app.currentBoard.name }} <img class="chevronIcon" [src]="'assets/images/icon-chevron-' + this.chevronIcon + '.svg'" alt=""></button>
+    <button class="boardName" [disabled]="app.innerWidth >= 768" mat-button (menuOpened)="this.chevronIcon = 'up'" (menuClosed)="this.chevronIcon = 'down'" [matMenuTriggerFor]="menu">{{ app.currentBoard.name }} <img class="chevronIcon" [src]="'assets/images/icon-chevron-' + this.chevronIcon + '.svg'" alt=""></button>
     <mat-menu xPosition="after" #menu="matMenu">
       <button (click)="app.changeCurrentBoard(board)" *ngFor="let board of app.data" mat-menu-item>{{ board.name }}</button>
     </mat-menu>
   </div>
   <div class="right">
-    <button class="addTaskBtn" [disabled]="this.app.boardEmpty"><img src="assets/images/icon-add-task-mobile.svg" alt=""></button>
+    <button class="addTaskBtn" [disabled]="this.app.boardEmpty"><img src="assets/images/icon-add-task-mobile.svg" alt=""><span class="addNewText">Add New Task</span></button>
     <button><img src="assets/images/icon-vertical-ellipsis.svg" alt=""></button>
   </div>
   `,
@@ -42,6 +42,31 @@ import { AppComponent } from '../app.component';
   }
   .right > button:last-child {
     padding: 3px 0 0 0;
+  }
+  .addNewText {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    .chevronIcon {
+      display: none;
+    }
+    .boardName:disabled {
+      cursor: none;
+    }
+    .left > img {
+      display: none;
+    }
+    .addNewText {
+      display: inline;
+      font-size: 15px;
+      font-weight: 600;
+      margin: 0 0 0 10px;
+    }
+    .addTaskBtn {
+      padding: 15px 25px;
+      border-radius: 30px;
+    }
   }
   `]
 })
