406d55e827c3481a0fa1739aa14133e5445980c3
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 20:01:02 -0500

    insert empty board component

--- src/app/components/board.component.ts
+++ src/app/components/board.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-board',
-  template: ``,
-  styles: [``]
+  template: `
+  <app-empty-board *ngIf="app.boardEmpty == true"></app-empty-board>
+  `,
+  styles: [`
+  app-empty-board {
+    display: grid;
+    padding: 0 20px;
+    align-self: center;
+    justify-self: center;
+    gap: 25px;
+  }
+  `]
 })
 export class BoardComponent {
-
+  constructor(public app: AppComponent) {}
 }
