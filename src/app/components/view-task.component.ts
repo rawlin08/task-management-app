4e6379a83d145c2c676e864ed955918d06f72943
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 22:00:14 -0500

    alpha task view

--- src/app/components/view-task.component.ts
+++ src/app/components/view-task.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-view-task',
-  template: ``,
-  styles: [``]
+  template: `
+  <div class="heading">
+    <h4>{{ app.currentTask.title }}</h4>
+  </div>
+  <p>{{ app.currentTask.description }}</p>
+  <div class="subtasks">
+    <p>Subtasks (2 of {{ app.currentTask.subtasks.length }})</p>
+    <div class="subtask" *ngFor="let subtask of app.currentTask.subtasks">
+      <p>{{ subtask.title }}</p>
+    </div>
+  </div>
+  <div class="status">
+    <p>Current Status</p>
+    
+  </div>
+  `,
+  styles: [`
+  .heading {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  `]
 })
 export class ViewTaskComponent {
-
+  constructor(public app: AppComponent) {}
 }
