9b12201f351cbec1d36b9a5442f0e3bea3060097
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 19:59:35 -0500

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
 import { SidebarComponent } from './components/sidebar.component';
 import { HeadingComponent } from './components/heading.component';
 import { ThemeToggleComponent } from './components/theme-toggle.component';
 import { BoardComponent } from './components/board.component';
 import { CardComponent } from './components/card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { EmptyBoardComponent } from './components/empty-board.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NewTaskComponent } from './components/new-task.component';
+import { EditTaskComponent } from './components/edit-task.component';
+import { ViewTaskComponent } from './components/view-task.component';
+import { NewBoardComponent } from './components/new-board.component';
+import { EditBoardComponent } from './components/edit-board.component';
+import { DeleteTaskComponent } from './components/delete-task.component';
+import { DeleteBoardComponent } from './components/delete-board.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +32,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HeadingComponent,
     ThemeToggleComponent,
     BoardComponent,
-    CardComponent
+    CardComponent,
+    EmptyBoardComponent,
+    NewTaskComponent,
+    EditTaskComponent,
+    ViewTaskComponent,
+    NewBoardComponent,
+    EditBoardComponent,
+    DeleteTaskComponent,
+    DeleteBoardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
