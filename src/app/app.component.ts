e5c9458ec3afaef4fa18b6808227697a639335e9
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 23:02:25 -0500

    sidebar display and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,24 @@ import * as todoData from 'src/assets/data.json';
   selector: 'app-root',
   template: `
   <mat-drawer-container class="example-container" [hasBackdrop]="false">
-    <mat-drawer #drawer mode="side">I'm a drawer</mat-drawer>
+    <mat-drawer #drawer mode="side">
+      <img class="logo" [src]="'assets/images/logo-light.svg'">
+      <div class="boards">
+        <h3>All Boards ({{ data.length }})</h3>
+        <button (click)="changeCurrentBoard(board)" class="board" *ngFor="let board of data">
+          <img src="assets/images/icon-board.svg" alt="">
+          <p>{{ board.name }}</p>
+        </button>
+        <button class="createNewBoardBtn">
+          <img class="boardIcon" src="assets/images/icon-board.svg" alt="">
+          <p>+ Create New Board</p>
+        </button>
+      </div>
+    </mat-drawer>
     <mat-drawer-content>
-      <button mat-raised-button (click)="drawer.toggle()">Toggle drawer</button>
       <main>
         <app-heading></app-heading>
+        <button mat-raised-button (click)="drawer.toggle()">Toggle drawer</button>
         <app-board></app-board>
       </main>
     </mat-drawer-content>
@@ -34,14 +47,63 @@ import * as todoData from 'src/assets/data.json';
     mat-drawer-container {
       display: block;
     }
+    mat-drawer {
+      background-color: var(--dark-gray);
+      border-right: 1px solid var(--black4);
+    }
     .mobile {
       display: none;
+    }
+    .logo {
+      padding: 32px 26px 0 26px;
+    }
+    h3 {
+      color: var(--gray);
+      font-size: 12px;
+      font-weight: 400;
+      letter-spacing: 2.4px;
+      text-transform: uppercase;
+      margin: 54px 0 19px 26px;
+    }
+    .board, .createNewBoardBtn {
+      display: flex;
+      align-items: center;
+      gap: 12px;
+      color: var(--gray);
+      width: 240px;
+      border-radius: 0 28px 28px 0;
+      padding: 0 26px;
+      margin: 0 20px 0 0;
+    }
+    .createNewBoardBtn {
+      padding: 0 10px 0 26px;
+      color: var(--purple);
+    }
+    .board > p, .createNewBoardBtn > p {
+      font-size: 15px;
+      font-weight: 500;
+      padding: 15px 0;
+    }
+    .selected {
+      background-color: var(--purple);
+      color: var(--white);
     }
   }
 
   /* DESKTOP STYLES */
   @media (min-width: 1281px) {
+    
+  }
 
+  @media (hover: hover) {
+    .board:hover {
+      background-color: var(--white);
+      color: var(--purple);
+    }
+    .createNewBoardBtn:hover {
+      background-color: var(--white);
+
+    }
   }
   `]
 })
@@ -65,6 +127,8 @@ export class AppComponent implements OnInit {
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.innerWidth = event.target.innerWidth;
+    console.log(this.innerWidth);
+    
   }
 
   changeCurrentBoard(board:any) {
