1be7fe13a8502a346057b2f9bbfbd6033e102766
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 20:00:48 -0500

    insert components and data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as todoData from 'src/assets/data.json';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <app-sidebar></app-sidebar>
+  <main>
+    <app-heading></app-heading>
+    <app-board></app-board>
+  </main>
+  `,
+  styles: [`
+  app-sidebar {
+    display: none;
+  }
+
+  /* TABLET STYLES */
+  @media (min-width: 768px) {
+    app-sidebar {
+      display: block;
+    }
+  }
+
+  /* DESKTOP STYLES */
+  @media (min-width: 1281px) {
+
+  }
+  `]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-management-app';
+  constructor() {}
+  ngOnInit(): void {
+    this.data = todoData;
+    this.data = this.data.boards;
+    console.log(this.data);
+  }
+
+  data:any;
+  boardEmpty:boolean = true;
 }
