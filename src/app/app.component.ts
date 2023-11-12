d9ef529eafab6a76d3633103f203fb77f0dc292b
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 22:00:01 -0500

    mobile styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import * as todoData from 'src/assets/data.json';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-sidebar></app-sidebar>
-  <main>
-    <app-heading></app-heading>
-    <app-board></app-board>
-  </main>
+  <mat-drawer-container class="example-container" [hasBackdrop]="false">
+    <mat-drawer #drawer mode="side">I'm a drawer</mat-drawer>
+    <mat-drawer-content>
+      <button mat-raised-button (click)="drawer.toggle()">Toggle drawer</button>
+      <main>
+        <app-heading></app-heading>
+        <app-board></app-board>
+      </main>
+    </mat-drawer-content>
+  </mat-drawer-container>
+  <div class="mobile">
+    <main>
+      <app-heading></app-heading>
+      <app-board></app-board>
+    </main>
+  </div>
   `,
   styles: [`
-  app-sidebar {
+  mat-drawer-container {
     display: none;
+  }
+  .mobile {
+    display: block;
   }
 
   /* TABLET STYLES */
   @media (min-width: 768px) {
-    app-sidebar {
+    mat-drawer-container {
       display: block;
+    }
+    .mobile {
+      display: none;
     }
   }
 
@@ -37,10 +54,21 @@ export class AppComponent implements OnInit {
     this.currentBoard = this.data.find((board:any) => board.id == 1);
     console.log(this.data);
     console.log(this.currentBoard);
-    
   }
 
   data:any;
   currentBoard:any;
+  currentTask:any;
   boardEmpty:boolean = false;
+  innerWidth:any;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = event.target.innerWidth;
+  }
+
+  changeCurrentBoard(board:any) {
+    this.currentBoard = board;
+    console.log(board);
+  }
 }
