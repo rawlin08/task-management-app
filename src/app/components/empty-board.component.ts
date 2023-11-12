8fbe1f3388c0b5d836b187c20116866b0fc2985f
Alec Rawlins <alec12999@yahoo.com>
2023-11-11 20:01:29 -0500

    empty board display

---
+++ src/app/components/empty-board.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-board',
+  template: `
+  <p>This board is empty. Create a new column to get started.</p>
+  <button>+ Add New Column</button>
+  `,
+  styles: [`
+  p {
+    color: var(--gray);
+    text-align: center;
+    font-size: 18px;
+    font-weight: 500;
+  }
+  button {
+    background-color: var(--purple);
+    border-radius: 24px;
+    padding: 15px 17px;
+    font-weight: 500;
+    justify-self: center;
+  }
+  `]
+})
+export class EmptyBoardComponent {
+
+}
