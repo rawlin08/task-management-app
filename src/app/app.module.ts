fe774fbd7cc0aec33cb394aa4b4b8e8b96aa55e5
Alec Rawlins <alec12999@yahoo.com>
2023-12-08 23:45:15 -0500

    import drag and drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 // Components
 import { HeadingComponent } from './components/heading.component';
