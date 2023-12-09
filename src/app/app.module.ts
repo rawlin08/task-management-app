abf6aa7d5a13a3da3937b98d95d8eef96e541ffc
Alec Rawlins <alec12999@yahoo.com>
2023-12-09 18:43:41 -0500

    delete unneeded things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,6 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HeadingComponent } from './components/heading.component';
 import { ThemeToggleComponent } from './components/theme-toggle.component';
 import { BoardComponent } from './components/board.component';
-import { EditTaskComponent } from './components/edit-task.component';
-import { ViewTaskComponent } from './components/view-task.component';
-import { NewBoardComponent } from './components/new-board.component';
-import { EditBoardComponent } from './components/edit-board.component';
-import { DeleteTaskComponent } from './components/delete-task.component';
-import { DeleteBoardComponent } from './components/delete-board.component';
 import { EmptyBoardComponent } from './components/empty-board.component';
 
 // Angular Material
@@ -41,12 +35,6 @@ import { EditTaskDialogComponent } from './components/dialogs/edit-task-dialog.c
     ThemeToggleComponent,
     BoardComponent,
     EmptyBoardComponent,
-    EditTaskComponent,
-    ViewTaskComponent,
-    NewBoardComponent,
-    EditBoardComponent,
-    DeleteTaskComponent,
-    DeleteBoardComponent,
     NewBoardDialogComponent,
     DeleteBoardDialogComponent,
     DeleteTaskDialogComponent,
