a47a876dcce5f45166c52196b81b7f824c270e4b
Alec Rawlins <alec12999@yahoo.com>
2023-12-09 22:00:45 -0500

    moved theme toggle to new component

--- src/app/components/theme-toggle.component.ts
+++ src/app/components/theme-toggle.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-theme-toggle',
-  template: ``,
+  template: `
+  <img src="assets/images/icon-light-theme.svg" alt="">
+  <mat-slide-toggle class="themeToggle" [checked]="app.darkMode" (change)="app.toggleTheme()"></mat-slide-toggle>
+  <img src="assets/images/icon-dark-theme.svg" alt="">
+  `,
   styles: [``]
 })
 export class ThemeToggleComponent {
-
+  constructor(public app: AppComponent) {}
 }
