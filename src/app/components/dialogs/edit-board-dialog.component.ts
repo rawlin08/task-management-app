9f74197d5ed19e40db96403f8740114548f0a033
Alec Rawlins <alec12999@yahoo.com>
2023-12-11 23:53:58 -0500

    form validation for editing board

--- src/app/components/dialogs/edit-board-dialog.component.ts
+++ src/app/components/dialogs/edit-board-dialog.component.ts
@@ -1,5 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-edit-board-dialog',
@@ -10,10 +19,13 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
       <form #form action="">
         <div class="name input">
           <label for="name">Board Name</label>
-          <input #boardName placeholder="e.g. Web Design" id="name" name="name" type="text" [value]="currentBoard.name">
+          <input required [ngClass]="titleFormControl.hasError('required') && titleFormControl.touched == true ? 'error' : ''" required matInput [errorStateMatcher]="matcher" [formControl]="titleFormControl" #boardName placeholder="e.g. Web Design" id="name" name="name" type="text" [value]="currentBoard.name">
+          <mat-hint>*required</mat-hint>
+          <mat-error *ngIf="titleFormControl.hasError('required') && titleFormControl.touched == true">Can't be empty</mat-error>
         </div>
         <div #columnNames class="columns input">
           <label>Board Columns</label>
+          <mat-hint class="one">*at least one (1) required</mat-hint>
           <div *ngFor="let column of currentBoard.columns">
             <input [(ngModel)]="column.name" id="editcolumn" [placeholder]="column.placeholder" [name]="column.id" type="text">
             <button type="button" (click)="deleteColumn(column.id)"><img src="assets/images/icon-cross.svg" alt=""></button>
@@ -23,11 +35,14 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
     </mat-dialog-content>
     <mat-dialog-actions class="editBoard">
       <button type="button" (click)="addColumn()">+ Add New Column</button>
-      <button mat-dialog-close="save" (click)="editBoard($event, form)">Save Changes</button>
+      <button [disabled]="getVal()" mat-dialog-close="save" (click)="editBoard($event, form)">Save Changes</button>
     </mat-dialog-actions>
   </div>
   `,
   styles: [`
+  .one {
+    margin: -5px 0 0 0;
+  }
   .columns > div {
     display: flex;
     justify-content: space-between;
@@ -46,8 +61,10 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
     font-size: 13px;
     font-weight: 500;
     border-radius: 20px;
-    padding: 8px;
+    padding: 10px;
     width: 100%;
+    opacity: 1;
+    transition: opacity 0.1s ease-in-out;
   }
   .editBoard > button:first-child {
     background-color: var(--white);
@@ -56,6 +73,10 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   .editBoard > button:last-child {
     background-color: var(--purple);
     color: var(--white);
+  }
+  .editBoard > button:last-child:disabled {
+    opacity: 0.3;
+    transition: opacity 0.2s ease-in-out;
   }
   #editcolumn {
     width: 100%;
@@ -66,12 +87,34 @@ export class EditBoardDialogComponent implements OnInit {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
   ngOnInit(): void {
     this.todoData = JSON.parse(localStorage.getItem('boards')!);
+    console.log(this.currentBoard.columns.length == 1);
+    
   }
-
-  todoData:any;
   currentBoard:any = JSON.parse(JSON.stringify(this.data));
+  titleFormControl = new FormControl(this.currentBoard.name, [Validators.required]);
+  columnNameFormControl = new FormControl('', [Validators.required]);
+  matcher = new MyErrorStateMatcher();
+
+  place:boolean = false;
+  todoData:any;
   board:any = {};
 
+  getVal() {
+    if (this.titleFormControl.hasError('required')) {
+      return true;
+    }
+    if (this.currentBoard.columns.length == 1) {
+      if (this.currentBoard.columns[0].name == '') {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+  }
   editBoard(e: Event, form:any) {
     e.preventDefault();
     this.board = {
