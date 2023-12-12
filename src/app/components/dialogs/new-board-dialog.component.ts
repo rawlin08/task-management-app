5204d818f42fa283d6d278be2b60ac1bf87cc4b4
Alec Rawlins <alec12999@yahoo.com>
2023-12-11 22:49:18 -0500

    give form validation for new board creation

--- src/app/components/dialogs/new-board-dialog.component.ts
+++ src/app/components/dialogs/new-board-dialog.component.ts
@@ -1,7 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-new-board-dialog',
+  encapsulation: ViewEncapsulation.None,
   providers: [],
   template: `
   <div class="dialog">
@@ -10,26 +20,38 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
       <form #form action="">
         <div class="name input">
           <label for="name">Board Name</label>
-          <input #boardName placeholder="e.g. Web Design" id="name" name="name" type="text">
+          <input required [ngClass]="titleFormControl.hasError('required') && titleFormControl.touched == true ? 'error' : ''" required matInput [errorStateMatcher]="matcher" [formControl]="titleFormControl" #boardName placeholder="e.g. Web Design" id="name" name="name" type="text">
+          <mat-hint>*required</mat-hint>
+          <mat-error *ngIf="titleFormControl.hasError('required') && titleFormControl.touched == true">Can't be empty</mat-error>
         </div>
         <div #columnNames class="columns input">
           <label>Board Columns</label>
           <div *ngFor="let column of newBoard.columns">
-            <input [(ngModel)]="column.name" id="columnName" placeholder="e.g. Todo" [name]="column.id" type="text">
+            <input required [ngClass]="columnNameFormControl.hasError('required') && columnNameFormControl.touched == true && column.id == 1 ? 'error' : ''" required matInput [errorStateMatcher]="matcher" [formControl]="column.id == 1 ? columnNameFormControl : place" [(ngModel)]="column.name" id="columnName" placeholder="e.g. Todo" [name]="column.id" type="text">
             <button type="button" (click)="deleteColumn(column.id)"><img src="assets/images/icon-cross.svg" alt=""></button>
+            <mat-hint *ngIf="column.id == 1">*required</mat-hint>
+            <mat-error *ngIf="columnNameFormControl.hasError('required') && columnNameFormControl.touched == true && column.id == 1">Can't be empty</mat-error>
           </div>
         </div>
       </form>
     </mat-dialog-content>
     <mat-dialog-actions class="newBoard">
       <button type="button" (click)="addColumn()">+ Add New Column</button>
-      <button mat-dialog-close="create" (click)="createBoard($event, form)">Create New Board</button>
+      <button [disabled]="titleFormControl.hasError('required') || columnNameFormControl.hasError('required')" mat-dialog-close="create" (click)="createBoard($event, form)">Create New Board</button>
     </mat-dialog-actions>
   </div>
   `,
   styles: [`
   .columns > div:nth-child(2) > button {
     display: none;
+  }
+  .columns > div:nth-child(2) {
+    display: grid;
+    justify-content: normal;
+    gap: 3px;
+  }
+  .columns > div:not(:nth-child(2)) > input {
+    width: 100%;
   }
   .columns > div {
     display: flex;
@@ -59,9 +81,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   .newBoard > button:last-child {
     background-color: var(--purple);
     color: var(--white);
+    opacity: 1;
+    transition: opacity 0.1s ease-in-out;
   }
-  #columnName {
-    width: 100%;
+  .newBoard > button:last-child:disabled {
+    opacity: 0.3;
+    transition: opacity 0.2s ease-in-out;
   }
   `]
 })
@@ -69,8 +94,13 @@ export class NewBoardDialogComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {
     this.todoData = JSON.parse(localStorage.getItem('boards')!);
+    console.log(this.columnNameFormControl);
   }
+  titleFormControl = new FormControl('', [Validators.required]);
+  columnNameFormControl = new FormControl('', [Validators.required]);
+  matcher = new MyErrorStateMatcher();
 
+  place:any;
   todoData:any;
   newBoard:any = {
     columns: [
@@ -85,23 +115,25 @@ export class NewBoardDialogComponent implements OnInit {
   createBoard(e:Event, form:any) {
     e.preventDefault();
   
-    const boardIDs = this.todoData.map((object:any) => {
-      return object.id;
-    })
-    let maxID:any;
-    if (boardIDs.length == 0) {
-      maxID = 0
+    if (!this.titleFormControl.hasError('required') && !this.columnNameFormControl.hasError('required')) {
+      const boardIDs = this.todoData.map((object:any) => {
+        return object.id;
+      })
+      let maxID:any;
+      if (boardIDs.length == 0) {
+        maxID = 0
+      }
+      else {
+        maxID = Math.max(...boardIDs);
+      }
+      this.newBoard = {
+        id: maxID + 1,
+        name: form.elements.name.value,
+        columns: this.newBoard.columns
+      }
+      this.todoData.push(this.newBoard);
+      this.updateLocalStorage();
     }
-    else {
-      maxID = Math.max(...boardIDs);
-    }
-    this.newBoard = {
-      id: maxID + 1,
-      name: form.elements.name.value,
-      columns: this.newBoard.columns
-    }
-    this.todoData.push(this.newBoard);
-    this.updateLocalStorage();
   }
   updateLocalStorage() {
     localStorage.setItem('boards', JSON.stringify(this.todoData));
